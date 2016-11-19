59b3e684599782b289de835d7251c87e79d52a78
kalmahik <kalmahik@yahoo.com>
2016-11-19 05:15:00 +0300

    1

--- app/src/main/java/ru/hackrussia/foodsharing1/CategoryListActivity.java
+++ app/src/main/java/ru/hackrussia/foodsharing1/CategoryListActivity.java
@@ -8,23 +8,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class CategoryListActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private CategoryListAdapter adapter;
-    private List<Category> category;
+    private List<Category> categories;
     private FloatingActionButton fab;
-    private Category ad;
+    private List<Ad> ads;
+    private Ad[] ada;
+
 
     private OnListItemClickListener clickListener = new OnListItemClickListener() {
         @Override
         public void onClick(View v, int position) {
+            doSignUp();
             Intent intent = new Intent(CategoryListActivity.this, CategoryListActivity.class);
-            intent.putExtra("title", category.get(position).getTitle());
+            intent.putExtra("title", categories.get(position).getTitle());
             startActivity(intent);
         }
     };
@@ -47,13 +60,13 @@ public class CategoryListActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         fab = (FloatingActionButton) findViewById(R.id.fab);
 
-        adapter = new CategoryListAdapter(category, clickListener);
+        adapter = new CategoryListAdapter(categories, clickListener);
         recyclerView.setAdapter(adapter);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(CategoryListActivity.this, AdListActivity.class);
+                Intent intent = new Intent(CategoryListActivity.this, CategoryListActivity.class);
                 startActivity(intent);
             }
         });
@@ -61,20 +74,49 @@ public class CategoryListActivity extends AppCompatActivity {
     }
 
     public void createFakeUsers() {
-        category = new ArrayList<>();
+        categories = new ArrayList<>();
 
         String[] category = new String[]{"Хлебобулочные изделия", "Кондитерские изделия", "Напитки",
                 "Замороженные продукты", "Фрукты и овощи", "Бакалея", "Молочные продукты", "Консервы", "Соусы",
                 "Полуфабрикаты", "Чай и кофе", "Прочее"};
 
         for (int i = 0; i < category.length; i++) {
-            this.category.add(new Category(category[i], "image"));
+            this.categories.add(new Category(category[i], "image"));
         }
     }
 
-
-
     public void onListChanged(int position) {
         adapter.notifyDataSetChanged();
+    }
+
+
+    private void doSignUp() {
+        //AuthPayload payload = new AuthPayload(username.getText().toString(), HashUtil.hash(password.getText().toString()));
+        //final RequestContainer<AuthPayload> requestContainer = new RequestContainer<>(payload);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient client = new OkHttpClient();
+                //RequestBody body = RequestBody
+                 //       .create(MediaType.parse("application/json; charset=utf-8"), new Gson().toJson(requestContainer));
+                Request request = new Request.Builder()
+                        .url("http://www.0a.by/json.php?cat=96")
+                        //.post(body)
+                        .build();
+                try {
+                    Response response = client.newCall(request).execute();
+                    Gson gson = new Gson();
+                    //ada = gson.fromJson(response.body().string(), Ad[].class);
+                    //AuthResponse ar = gson.fromJson(rc.getPayload().toString(), AuthResponse.class);
+                    //Log.d(LoginActivity.class.getSimpleName(), ar.getUser().getName());
+                    //Log.d(CategoryListActivity.class.getSimpleName(), ada[0].toString());
+
+                    Log.d(CategoryListActivity.class.getSimpleName(), response.body().string());
+
+                } catch (IOException e) {
+                    Log.e(CategoryListActivity.class.getSimpleName(), Log.getStackTraceString(e));
+                }
+            }
+        }).start();
     }
 }
